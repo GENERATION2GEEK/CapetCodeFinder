b5f5895299eede2bf2a2d223ddf8e69c35699015
Vincent MOUCADEAU <generation2geek@gmail.com>
2021-09-28 23:26:55 +0200

    changement librairie imap

--- index.js
+++ index.js
@@ -1,84 +1,37 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+var fs      = require('fs');
+var downloadEmailAttachments = require('download-email-attachments');
+
+var lastcode = "";
+
+var onEnd = function(result) {
+  if(result.error) { 
+    console.log(result.error);
+    return
+  }
+  console.log("Done");
+  console.log(result.latestTime);
+};
 
 
+function getFacture(account) {
+  var date = new Date();
 
-var Imap = require('imap'),
-    inspect = require('util').inspect;
-const { forEach } = require('lodash');
-
-var imap = new Imap({
-    user: 'v.moucadeau@gmail.com',
-    password: '',
-    host: 'imap.gmail.com',
-    servername: 'imap.gmail.com',
-    port: 993,
-    tls: true
-    });
-
-
-var pdfParser = require('pdf-parser');
-
-var PDF_PATH = 'facture.pdf';
-
-pdfParser.pdf2json(PDF_PATH, function (error, pdf) {
-    if(error != null){
-        console.log(error);
-    }else{
-        var page0 = pdf["pages"][0];
-        page0.forEach(function(element) {
-            console.log(element);
-        })
-    }
-});
-
-
-function openInbox(cb) {
-  //imap.openBox('INBOX', true, cb);
-  return
+  var datestr = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + "27";
+  
+  downloadEmailAttachments({
+    //invalidChars: /[^A-Z]/g, //Regex of Characters that are invalid and will be replaced by X
+    account: '"v.moucadeau@gmail.com":@imap.gmail.com', // all options and params besides account are optional
+    filenameTemplate: 'facture.pdf',
+    filenameFilter: /.pdf?$/,
+    timeout: 3000,
+    since: datestr,
+    log: {warn: console.warn, debug: console.info, error: console.error, info: console.info },
+    attachmentHandler: function (attachmentData, callback, errorCB) {
+      console.log(attachmentData)
+      callback()
+    },
+  }, onEnd)
+  
 }
 
-
-
-
-imap.once('ready', function() {
-    openInbox(function(err, box) {
-        if (err) throw err;
-        imap.search([ ['FROM', 'ne-pas-repondre@cap-etudes.com'] ], function(err, results) {
-          if (err) throw err;
-          var f = imap.fetch(results, { bodies: '' });
-          f.on('message', function(msg, seqno) {
-            console.log('Message #%d', seqno);
-            var prefix = '(#' + seqno + ') ';
-            msg.on('body', function(stream, info) {
-                var buffer = '';
-                stream.on('data', function(chunk) {
-                  buffer += chunk.toString('utf8');
-                });
-                stream.once('end', function() {
-                  console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-                });
-              });
-            msg.once('end', function() {
-              console.log(prefix + 'Finished');
-            });
-          });
-          f.once('error', function(err) {
-            console.log('Fetch error: ' + err);
-          });
-          f.once('end', function() {
-            console.log('Done fetching all messages!');
-            imap.end();
-          });
-        });
-      });
-});
-
-imap.once('error', function(err) {
-  console.log(err);
-});
-
-imap.once('end', function() {
-  console.log('Connection ended');
-});
-
-imap.connect();
