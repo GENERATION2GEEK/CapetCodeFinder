486206cb98a9ce565108a725a642c4611d0d67e4
Vincent MOUCADEAU <generation2geek@gmail.com>
2021-10-13 17:17:36 +0200

    test

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var downloadEmailAttachments = require('download-email-attachments');
 
 var lastcode = "";
 
+function delFacture() {
+  // delete a file
+  try {
+    fs.unlinkSync('facture.pdf');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 var onEnd = function(result) {
   if(result.error) { 
     console.log(result.error);
@@ -13,19 +22,38 @@ var onEnd = function(result) {
   console.log(result.latestTime);
 };
 
+function getLastCode(file) {
+  var pdfParser = require('pdf-parser');
+
+
+  pdfParser.pdf2json(file, function (error, pdf) {
+      if(error != null){
+          console.log(error);
+      }else{
+          var page0 = pdf["pages"][0];
+          page0.forEach(function(element) {
+              console.log(element);
+          })
+      }
+  });
+
+}
+
 
 function getFacture(account) {
+  delFacture();
   var date = new Date();
 
   var datestr = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + "27";
   
   downloadEmailAttachments({
     //invalidChars: /[^A-Z]/g, //Regex of Characters that are invalid and will be replaced by X
-    account: '"v.moucadeau@gmail.com":@imap.gmail.com', // all options and params besides account are optional
+    account: '"v.moucadeau@gmail.com":pbkfqnwmcdocqrfc@imap.gmail.com', // all options and params besides account are optional
     filenameTemplate: 'facture.pdf',
     filenameFilter: /.pdf?$/,
     timeout: 3000,
     since: datestr,
+    sender: "e-pas-repondre@capetude.com",
     log: {warn: console.warn, debug: console.info, error: console.error, info: console.info },
     attachmentHandler: function (attachmentData, callback, errorCB) {
       console.log(attachmentData)
@@ -35,3 +63,4 @@ function getFacture(account) {
   
 }
 
+getFacture("loegjl");
