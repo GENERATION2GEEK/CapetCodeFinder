2fb69e14048d2b204ccfa1b6ee8132a069b3987f
Vincent Moucadeau <v.moucadeau@gmail.com>
2022-05-08 22:16:25 +0200

    Add lastcode save Parse code change date

--- index.js
+++ index.js
@@ -4,54 +4,78 @@ var downloadEmailAttachments = require('download-email-attachments');
 var cron = require('node-cron');
 var pdfParser = require('pdf-parser');
 const https = require('https');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const { Client, Intents } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
-
 var lastcode = "";
-var facture_day = "29";
+if (fs.existsSync("lastcode.txt")) {
+  lastcode = fs.readFileSync("lastcode.txt", "utf8");
+}
+else {
+  fs.writeFileSync("lastcode.txt", lastcode);
+}
+console.log(lastcode);
 
-var gmail_account = '"email@gmail.com":password@imap.gmail.com'; // your gmail account
 
-var cron_download_facture = '0 * * ' + facture_day + ' * *'; // facture day at 00:00:00
+var facture_day = process.env.FACTURE_DAYS;
 
-var cron_reminder = '0 0 8 10 * *'; // 8h du matin le 10 du mois
-// cron_reminder = '0 49 12 12 * *'; // DEBUG
+var email = process.env.GMAIL_ID;
+var password = process.env.GMAIL_APP_PASS;
 
-var discord_user_id = "YOUR_USER_ID"; // discord user id
+var gmail_account = '"' + email + '":' + password + '@imap.gmail.com';
+//var cron_download_facture = '0 * * ' + facture_day + ' * *'; // facture day at 00:00:00
+cron_download_facture = '0 * * * * *'; // facture day at 00:00:00
+
+
+
+var discord_users = process.env.DISCORD_USERS.split(",");
+
+var cron_reminder = '0 * * * * *'; // rappel à 7h du matin
+
+function parseFactureDate(codestr) {
+  var monthsarray = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+  codestr = codestr.split(' ');
+  var day = codestr[10];
+  var monthname = codestr[11];
+  var year = codestr[12];
+  var month = monthsarray.indexOf(monthname) + 1;
+  var date = new Date(year, month, day);
+  return date;
+}
+
+async function send_discord_msg(message) {
+  for (const id of discord_users) {
+    var discord_user = await client.users.fetch(id);
+    discord_user = await discord_user.createDM();
+    discord_user.send(message);
+  }
+}
 
 // Send a sms message through the Free Mobile API
 async function send_sms(message) {
   message_encoded = encodeURIComponent(message);
-  var url = "https://smsapi.free-mobile.fr/sendmsg?user=USERID&pass=PASS&msg=" + message_encoded;
+  var url = "https://smsapi.free-mobile.fr/sendmsg?user=" + process.env.FREE_SMS_USERID + "&pass=" + process.env.FREE_SMS_PASSKEY + "&msg=" + message_encoded;
   https.get(url, (resp) => {}).on("error", (err) => {
     console.log("Error: " + err.message);
   });
 }
 
-client.on('ready', async() => {
+client.on('ready', () => {
 	console.log('Ready!');
-
-  var discord_user = await client.users.fetch(discord_user_id);
-  discord_user = await discord_user.createDM();
-
-  
-  
   cron.schedule(cron_download_facture, () => {
     var date = new Date();
-
-    // var datestr = "2021" + "-" + "12" + "-" + "26"; // DEBUG
+  
     var datestr = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + (date.getDate()-5);
+    datestr = "2022" + "-" + "04" + "-" + "26"; // DEBUG
     getFacture(datestr, gmail_account, function(result) {
       if(result.error) { 
         console.log(result.error);
-        discord_user.send("Erreur : " + result.error);
         return
       }
-      console.log("Done");
-      console.log(result.latestTime);
       pdfParser.pdf2json("facture.pdf", function(error, pdf) {
         if(error != null){
           console.log(error);
@@ -63,9 +87,11 @@ client.on('ready', async() => {
                 var new_code = element["text"];
                 if(lastcode != new_code) {
                   lastcode = new_code;
-                  // console.log("New code : " + lastcode); // debug
-                  discord_user.send(lastcode);
+                  console.log("New code : " + lastcode); // debug
+                  send_discord_msg(lastcode);
                   send_sms(lastcode);
+                  
+                  fs.writeFileSync("lastcode.txt", lastcode);
                 }
             
               }
@@ -82,8 +108,11 @@ client.on('ready', async() => {
 });
 
 cron.schedule(cron_reminder, () =>  { // Rappel du code avant le changement
-  if(lastcode != "") {
-    discord_user.send("Rappel : " + lastcode);
+  if (lastcode == "") {return;}
+  var change_date = parseFactureDate(lastcode);
+  var datenow = new Date();
+  if (change_date.getDay() == datenow.getDay()) {
+    send_discord_msg("Rappel : " + lastcode);
     send_sms("Rappel : " + lastcode);
   }
 });
@@ -92,7 +121,7 @@ function delFacture() {
   try {
     fs.unlinkSync('facture.pdf');
   } catch (error) {
-    console.log(error);
+    // console.log(error);
   }
 }
 
@@ -118,4 +147,4 @@ function getFacture(datestr, account, callback) {
   
 }
 
-client.login('TOKEN');
+client.login(process.env.DISCORD_TOKEN);
