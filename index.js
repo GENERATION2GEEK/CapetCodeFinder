f6e93eb65d1c3439151c3214e37fd9338185cddb
Vincent Moucadeau <v.moucadeau@gmail.com>
2023-09-03 13:09:31 +0200

    bugfix in progress

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ var fs      = require('fs');
 var downloadEmailAttachments = require('download-email-attachments');
 var cron = require('node-cron');
 var pdfParser = require('pdf-parser');
-const https = require('https');
+
+
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { Client, Intents } = require('discord.js');
+const moment = require('moment');
+const MyImap = require('./myimap');
+const logger = require('pino')({
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            translateTime: false,
+            colorize: true,
+            ignore: 'pid,hostname,time',
+        },
+    },
+});
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
 var lastcode = "";
 if (fs.existsSync("lastcode.txt")) {
@@ -21,7 +32,7 @@ else {
 console.log(lastcode);
 
 
-var facture_day = process.env.FACTURE_DAYS;
+var facture_day = process.env.FACTURE_DAY;
 
 var email = process.env.GMAIL_ID;
 var password = process.env.GMAIL_APP_PASS;
@@ -29,90 +40,103 @@ var password = process.env.GMAIL_APP_PASS;
 var gmail_account = '"' + email + '":' + password + '@imap.gmail.com';
 var cron_download_facture = '0 * * ' + facture_day + ' * *'; // facture day at 00:00:00
 
+var ntfyserver = process.env.NTFY_SERVER;
+var ntfytoken = process.env.NTFY_TOKEN;
 
-
-var discord_users = process.env.DISCORD_USERS.split(",");
 
 var cron_reminder = '0 0 7 * * *'; // rappel à 7h du matin
 
 function parseFactureDate(codestr) {
-  var monthsarray = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+  codestr = codestr.toLowerCase();
   codestr = codestr.split(' ');
-  var day = parseInt(codestr[10], 10);
-  var monthname = codestr[11];
-  var year = codestr[12];
-  var month = monthsarray.indexOf(monthname);
-  var date = new Date(year, month, day+1, 0, 0, 10);
-  return date;
-}
 
-async function send_discord_msg(message) {
-  for (const id of discord_users) {
-    var discord_user = await client.users.fetch(id);
-    discord_user = await discord_user.createDM();
-    discord_user.send(message);
+  var monthsarray = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+  var days_array = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+
+  var change_day;
+  var change_daynumb;
+  var change_month;
+  var change_year;
+
+  for(day in days_array) {
+    var daypos = codestr.findIndex(days_array[day]);
+    change_day = days_array[day];
+    if(daypos != -1) {
+      change_daynumb = codestr[daypos+1];
+      change_month = codestr[daypos+2];
+      change_year = codestr[daypos+3];
+      var date = new Date(change_year, monthsarray.indexOf(change_month), change_daynumb, 0, 0, 10);
+      return date;
+    }
   }
-}
-
-// Send a sms message through the Free Mobile API
-async function send_sms(message) {
-  message_encoded = encodeURIComponent(message);
-  var url = "https://smsapi.free-mobile.fr/sendmsg?user=" + process.env.FREE_SMS_USERID + "&pass=" + process.env.FREE_SMS_PASSKEY + "&msg=" + message_encoded;
-  https.get(url, (resp) => {}).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
-}
-
-client.on('ready', () => {
-	console.log('Ready!');
-  cron.schedule(cron_download_facture, () => {
-    var date = new Date();
+  return false;
   
-    var datestr = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + (date.getDate()-5);
-    datestr = "2022" + "-" + "04" + "-" + "26"; // DEBUG
-    getFacture(datestr, gmail_account, function(result) {
-      if(result.error) { 
-        console.log(result.error);
-        return
-      }
-      pdfParser.pdf2json("facture.pdf", function(error, pdf) {
-        if(error != null){
-          console.log(error);
-        }else{
-          var page0 = pdf["pages"][0]["texts"];
-          
-          page0.forEach(function(element) {
-              if(element["text"].startsWith("Le code")) {
-                var new_code = element["text"];
-                if(lastcode != new_code) {
-                  lastcode = new_code;
-                  console.log("New code : " + lastcode); // debug
-                  send_discord_msg(lastcode);
-                  send_sms(lastcode);
-                  
-                  fs.writeFileSync("lastcode.txt", lastcode);
-                }
-            
+}
+
+// Post ntfy message
+async function post_ntfy(message) {
+  fetch(ntfyserver + "/capetude", {
+    method: 'POST', // PUT works too
+    body: message,
+    headers: {
+        'Authorization': 'Bearer ' + ntfytoken,
+    }
+})
+}
+
+cron.schedule(cron_download_facture, () => {
+  var date = new Date();
+  var day = date.getTime() - (1000 * 60 * 60 * 24 * 5); // 5 days ago
+  date.setTime(day);
+
+  var monthstr = (date.getMonth+1) < 10 ? "0" + (date.getMonth()+1) : (date.getMonth()+1);
+  var daystr = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+
+  var datestr = date.getFullYear() + "-" + monthstr + "-" + daystr;
+  datestr = "2023" + "-" + "08" + "-" + "26"; // DEBUG
+  getFacture(datestr, gmail_account, function(result) {
+    if(result.error) { 
+      console.log(result.error);
+      return
+    }
+    pdfParser.pdf2json("facture.pdf", function(error, pdf) {
+      if(error != null){
+        console.log(error);
+      }else{
+        var page0 = pdf["pages"][0]["texts"];
+        
+        page0.forEach(function(element) {
+            if(element["text"].startsWith("Le code")) {
+              var new_code = element["text"];
+              if(lastcode != new_code) {
+                lastcode = new_code;
+                console.log("New code : " + lastcode); // debug
+                post_ntfy(lastcode);
+                
+                fs.writeFileSync("lastcode.txt", lastcode);
               }
-          });
-        }
-      });
-      
+          
+            }
+        });
+      }
     });
-      
+    
   });
-  
-  
-
+    
 });
 
 cron.schedule(cron_reminder, () =>  { // Rappel du code avant le changement
   if (lastcode == "") {return;}
   var change_date = parseFactureDate(lastcode);
+  if(change_date == false) {
+    if(datenow.getDate() in [1,3,10]) {
+      post_ntfy("Rappel : " + lastcode);
+    }
+    return;
+  }
   var datenow = new Date();
   if (change_date.getDate() == datenow.getDate()) {
-    send_discord_msg("Rappel : " + lastcode);
-    send_sms("Rappel : " + lastcode);
+    post_ntfy("Rappel : " + lastcode);
   }
 });
 
@@ -136,6 +160,7 @@ function getFacture(datestr, account, callback) {
     filenameFilter: /.pdf?$/,
     timeout: 3000,
     since: datestr,
+    ssl: true,
     sender: "ne-pas-repondre@cap-etudes.com", // The library must be modified to support this option
     log: {warn: console.warn, debug: console.info, error: console.error, info: console.info },
     attachmentHandler: function (attachmentData, callback, errorCB) {
@@ -146,4 +171,5 @@ function getFacture(datestr, account, callback) {
   
 }
 
-client.login(process.env.DISCORD_TOKEN);
+
+console.log(gmail_account);
